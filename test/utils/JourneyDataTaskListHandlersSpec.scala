f7d75097b8726fc8c9e7e50ebab088f6cfec45b4
Tapiwa Tiyemba <57238297+ttiyemba@users.noreply.github.com>
2025-12-19 11:10:57 +0000

    DFI-944 (#10)
    * DFI-944
    
    * DFI-944

--- test/utils/JourneyDataTaskListHandlersSpec.scala
+++ test/utils/JourneyDataTaskListHandlersSpec.scala
@@ -53,7 +53,8 @@ class JourneyDataTaskListHandlersSpec extends BaseUnitSpec {
               registeredToManageIsa = Some(true),
               zRefNumber = Some("Z1111"),
               tradingUsingDifferentName = Some(true),
-              tradingName = Some(testString)
+              tradingName = Some(testString),
+              fcaNumber = Some("123456")
             )
             val json         = Json.toJson(original)(handler.writes.asInstanceOf[Writes[OrganisationDetails]])
             val deserialized = json.as(handler.reads.asInstanceOf[Reads[OrganisationDetails]])
