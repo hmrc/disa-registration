941a88c30073b6feaabc20b38abc6386c3be055f
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-08-07 07:15:10 +0100

    DFI-2296 updated CT UTR to be generalised UTR

--- app/uk/gov/hmrc/disaregistration/models/journeyData/BusinessVerification.scala
+++ app/uk/gov/hmrc/disaregistration/models/journeyData/BusinessVerification.scala
@@ -21,7 +21,6 @@ import play.api.libs.json.{Json, OFormat}
 case class BusinessVerification(
   businessRegistrationPassed: Option[Boolean],
   businessVerificationPassed: Option[Boolean],
-  // CT UTR for incorporated entities, SA UTR for partnerships.
   utr: Option[String],
   registeredAddress: Option[RegisteredAddress],
   companyName: Option[String],
